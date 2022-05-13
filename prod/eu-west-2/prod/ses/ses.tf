e5d8bfa1819b688163a249b5854d1b52ecd4a661
Bob Rohan <bob.rohan@nahor.co.uk>
2022-05-13 15:51:42 +0100

    renovate test

--- prod/eu-west-2/prod/ses/ses.tf
+++ prod/eu-west-2/prod/ses/ses.tf
@@ -1,3 +1,8 @@
+locals{
+  // amiFilter=[{"Name":"owner-id","Values":["602401143452"]},{"Name":"name","Values":["amazon-eks-node-1.21-*"]}]
+  ami_test= "ami-02ce3d9008cab69cb"
+}
+
 module "ses" {
 
   source = "git@github.com:nahor-co/terraform-modules.git//aws/ses?ref=0.0.4"
