69708c386b18732f629c9d37843d68a70315ffc0
Bob Rohan <bob.rohan@nahor.co.uk>
2022-05-17 16:27:03 +0100

    fmt

--- prod/eu-west-2/prod/ses/ses.tf
+++ prod/eu-west-2/prod/ses/ses.tf
@@ -1,7 +1,7 @@
 module "ses" {
 
   source = "git@github.com:nahor-co/terraform-modules.git//aws/ses?ref=0.0.5"
-//  source = "../../../../../terraform-modules/aws/ses"
+  //  source = "../../../../../terraform-modules/aws/ses"
 
   domain = "nahor.co.uk"
 
