0283fac0df0d00eaa8df14d6951ae29abfc52945
Bob Rohan <bob.rohan@nahor.co.uk>
2023-01-31 15:57:10 +0000

    demo-precommit-fail

--- prod/_global/hosted_zones/hosted_zones.tf
+++ prod/_global/hosted_zones/hosted_zones.tf
@@ -8,7 +8,7 @@ module "hosted_zone" {
 
   for_each = local.domains
 
-  source = "git@github.com:nahor-co/terraform-modules.git//aws/hosted_zone?ref=0.0.5"
+source = "git@github.com:nahor-co/terraform-modules.git//aws/hosted_zone?ref=0.0.5"
   #source = "../../../../terraform-modules/aws/hosted_zone"
 
   domain = each.value
